5f7165a689cf19f901a18ea926610717fa1b1651
karthik-code-ninja <kkarthikdcse@gmail.com>
2026-07-31 17:15:11 +0530

    schedule update

--- app/Http/Controllers/backend/CandidateController.php
+++ app/Http/Controllers/backend/CandidateController.php
@@ -196,6 +196,7 @@ class CandidateController extends Controller
     {
         $closureLevels = [
             InterviewLevel::CANDIDATE_NOT_INTERESTED_ID => 'Candidate Not Interested',
+            InterviewLevel::CANDIDATE_NOT_POSITION_ID => 'Position Hold',
             InterviewLevel::CANDIDATE_NOT_RESPONDING_ID => 'Candidate Not Responding',
         ];
 
