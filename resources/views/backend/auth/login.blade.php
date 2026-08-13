3ff2bc99162fff0ce6471dd2609407edf65462aa
karthik-code-ninja <kkarthikdcse@gmail.com>
2026-08-13 08:11:02 +0530

    admin only change password

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-    <link rel="shortcut icon" href="{{ asset('frontend/images/glow-unlock-favicon.png') }}">
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/images/favicon.png') }}">
     @include('backend.layouts.css_master')
     <link rel="stylesheet" href="{{ asset('vendor/flasher/flasher.min.css') }}">
 
